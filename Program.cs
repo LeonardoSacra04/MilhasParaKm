59c27245f55884d4d7df4aff850b37ac251efbf3
LeonardoSacra04 <oliveiraleo04@gmail.com>
2025-04-03 19:32:30 -0300

    transforma as milhas dadas pelo usuário em quilômetros

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double milhas;
+double km;
+
+
+Console.Clear();
+
+Console.WriteLine("Digite a quantidade de milhas a ser convertida em Km: ");
+milhas = Convert.ToDouble(Console.ReadLine()!);
+
+km = milhas * 1.609;
+
+Console.Clear();
+
+Console.WriteLine($"Medida em milhas: {milhas}");
+Console.WriteLine($"Medida em quilômetros: {km}");
